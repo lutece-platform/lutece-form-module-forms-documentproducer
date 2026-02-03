05061b5220174e7b7df5481b6d2dfbcd15c776f1
evdngsl <158455076+evdngsl@users.noreply.github.com>
2026-02-03 16:54:41 +0100

    LUT-31836 : V8 - BO : The document export configuration management generates an internal error

--- src/java/fr/paris/lutece/plugins/forms/modules/documentproducer/web/DocumentProducerJspBean.java
+++ src/java/fr/paris/lutece/plugins/forms/modules/documentproducer/web/DocumentProducerJspBean.java
@@ -48,7 +48,7 @@ import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import jakarta.servlet.http.HttpServletRequest;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import fr.paris.lutece.plugins.forms.modules.documentproducer.business.producerconfig.ConfigProducer;
 import fr.paris.lutece.plugins.forms.modules.documentproducer.business.producerconfig.DocumentType;
